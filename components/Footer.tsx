1ff81cc5463ed61f22960cb24d18bfba90fe00cb
Roshan Praveen Shetty <roshanshetty2000@gmail.com>
2024-08-16 09:41:02 -0400

    Footer implemented

---
+++ components/Footer.tsx
@@ -0,0 +1,52 @@
+import { socialMedia } from "@/data";
+import Image from "next/image";
+import { FaLocationArrow } from "react-icons/fa";
+import MagicButton from "./ui/MagicButton";
+
+const Footer = () => {
+	return (
+		<footer className="w-full pb-10" id="contact">
+			<div className="flex flex-col items-center">
+				<h1 className="heading lg:max-w-[45vw]">
+					Have a <span className="text-red-700">feature</span> in mind?
+				</h1>
+				<p className="text-white-200 md:mt-10 my-5 text-center">
+					We’re always looking to improve! Let’s
+					work together to make Netflix AutoSkip even better.
+				</p>
+				<a href="mailto:roshanshetty2000@gmail.com">
+					<MagicButton
+						title="Let&apos;s get in touch"
+						icon={<FaLocationArrow />}
+						position="right"
+					/>
+				</a>
+			</div>
+
+			<div className="flex mt-16 md:flex-row flex-col justify-between items-center">
+				<p className="md:text-base text-sm md:font-normal font-light">
+					Copyright © 2024 Roshan Praveen Shetty
+				</p>
+				<div className="flex items-center md:gap-3 gap-6 mt-5">
+					{socialMedia.map((profile) => (
+						<a href={profile.link} key={profile.id}>
+							<div
+								key={profile.id}
+								className="w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-150 bg-opacity-75 bg-black-200 rounded-lg border border-black-300"
+							>
+								<Image
+									src={profile.img}
+									alt={profile.id.toString()}
+									width={20}
+									height={20}
+								/>
+							</div>
+						</a>
+					))}
+				</div>
+			</div>
+		</footer>
+	);
+};
+
+export default Footer;
---
+++ data/index.ts
@@ -0,0 +1,284 @@
+export const navItems = [
+	{ name: "About", link: "#about" },
+	{ name: "Projects", link: "#projects" },
+	{ name: "References", link: "#references" },
+	{ name: "Experience", link: "#experience" },
+	{ name: "Contact", link: "#contact" },
+];
+
+export const gridItems = [
+	{
+		id: 1,
+		title: "Strong problem-solving skills and analytical thinking.",
+		description: "",
+		className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+		imgClassName: "w-full h-full",
+		titleClassName: "justify-end",
+		img: "/b1.svg",
+		spareImg: "",
+	},
+	{
+		id: 2,
+		title: "Effective communication and teamwork abilities.",
+		description: "",
+		className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+		imgClassName: "",
+		titleClassName: "justify-start",
+		img: "",
+		spareImg: "",
+	},
+	{
+		id: 3,
+		title: "My tech stack",
+		description: "I constantly try to improve",
+		className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+		imgClassName: "",
+		titleClassName: "justify-center",
+		img: "",
+		spareImg: "",
+	},
+	{
+		id: 4,
+		title: "Time management and meeting deadlines reliably.",
+		description: "",
+		className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+		imgClassName: "",
+		titleClassName: "justify-start",
+		img: "/grid.svg",
+		spareImg: "/b4.svg",
+	},
+
+	{
+		id: 5,
+		title: "Building a photography portfolio platform.",
+		description: "The Inside Scoop",
+		className: "md:col-span-3 md:row-span-2",
+		imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+		titleClassName: "justify-center md:justify-start lg:justify-center",
+		img: "/b5.svg",
+		spareImg: "/grid.svg",
+	},
+	{
+		id: 6,
+		title: "Want more details? Download my resume.",
+		description: "",
+		className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+		imgClassName: "",
+		titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+		img: "",
+		spareImg: "",
+	},
+];
+
+export const projects = [
+	{
+		id: 1,
+		title: "Exploring Moments",
+		des: "A customizable photography portfolio platform with client galleries, proofing, and online sales.",
+		iconLists: [
+			"/next.svg",
+			"/tail.svg",
+			"/ts.svg",
+			"/dock.svg",
+			"/fm.svg",
+			"/c.svg",
+			"git.svg",
+		],
+		img: "/p1.jpeg",
+		link: "https://roshanshetty.mypixieset.com/",
+	},
+	{
+		id: 2,
+		title: "RateMyDining",
+		des: "A mobile app for university students to access real-time dining reviews, ratings, and discover diverse campus food options.",
+		img: "/p2.jpg",
+		iconLists: [
+			"/re.svg",
+			"/tail.svg",
+			"/ts.svg",
+			"/stream.svg",
+			"/c.svg",
+			"git.svg",
+		],
+		link: "https://github.com/RoshanPShetty/ratemydining",
+	},
+	{
+		id: 3,
+		title: "RideShare",
+		des: "A web application that allows students to share rides to common destinations through a sophisticated portal.",
+		img: "/p3.jpg",
+		iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "git.svg", "/c.svg"],
+		link: "https://github.com/F-in-the-Chat/RideShare",
+	},
+	{
+		id: 4,
+		title: "Interviews Commanility",
+		des: "A mobile app aimed at streamlining witness interviews for FBI child rescue operations using Machine Learning.",
+		img: "/p4.jpg",
+		iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "git.svg"],
+		link: "https://github.com/ML4CR/InterviewsCommonality",
+	},
+];
+
+export const references = [
+	{
+		quote:
+			"I highly endorse Roshan Shetty as a Frontend Developer. His ability to exceed expectations and his independent, driven spirit made him a valuable asset during his internship. Roshan's proficiency in JavaScript/TypeScript, Svelte, and CSS, along with his leadership in UI development and testing, sets him apart. He would be a great fit for any team.",
+		name: "Michael Gulik",
+		title: "Software Engineer at GRUBBRR",
+		img: "/profile.jpg",
+	},
+	{
+		quote:
+			"I highly endorse Roshan Shetty as a Frontend Developer. His ability to exceed expectations and his independent, driven spirit made him a valuable asset during his internship. Roshan's proficiency in JavaScript/TypeScript, Svelte, and CSS, along with his leadership in UI development and testing, sets him apart. He would be a great fit for any team.",
+		name: "Michael Gulik",
+		title: "Software Engineer at GRUBBRR",
+		img: "/profile.jpg",
+	},
+	{
+		quote:
+			"I highly endorse Roshan Shetty as a Frontend Developer. His ability to exceed expectations and his independent, driven spirit made him a valuable asset during his internship. Roshan's proficiency in JavaScript/TypeScript, Svelte, and CSS, along with his leadership in UI development and testing, sets him apart. He would be a great fit for any team.",
+		name: "Michael Gulik",
+		title: "Software Engineer at GRUBBRR",
+		img: "/profile.jpg",
+	},
+	{
+		quote:
+			"I highly endorse Roshan Shetty as a Frontend Developer. His ability to exceed expectations and his independent, driven spirit made him a valuable asset during his internship. Roshan's proficiency in JavaScript/TypeScript, Svelte, and CSS, along with his leadership in UI development and testing, sets him apart. He would be a great fit for any team.",
+		name: "Michael Gulik",
+		title: "Software Engineer at GRUBBRR",
+		img: "/profile.jpg",
+	},
+	{
+		quote:
+			"I highly endorse Roshan Shetty as a Frontend Developer. His ability to exceed expectations and his independent, driven spirit made him a valuable asset during his internship. Roshan's proficiency in JavaScript/TypeScript, Svelte, and CSS, along with his leadership in UI development and testing, sets him apart. He would be a great fit for any team.",
+		name: "Michael Gulik",
+		title: "Software Engineer at GRUBBRR",
+		img: "/profile.jpg",
+	},
+];
+
+export const companies = [
+	{
+		id: 1,
+		name: "cloudinary",
+		img: "/cloud.svg",
+		nameImg: "/cloudName.svg",
+	},
+	{
+		id: 2,
+		name: "appwrite",
+		img: "/app.svg",
+		nameImg: "/appName.svg",
+	},
+	{
+		id: 3,
+		name: "HOSTINGER",
+		img: "/host.svg",
+		nameImg: "/hostName.svg",
+	},
+	{
+		id: 4,
+		name: "stream",
+		img: "/s.svg",
+		nameImg: "/streamName.svg",
+	},
+	{
+		id: 5,
+		name: "docker.",
+		img: "/dock.svg",
+		nameImg: "/dockerName.svg",
+	},
+];
+
+export const workExperience = [
+	{
+		id: 1,
+		company: "Tata Consultancy Services Limited",
+		title: "Software Engineer",
+		dateStart: "Oct. 2023",
+		dateEnd: "Present",
+		desc: "Coordinated with 5+ teams at Kaiser Permanente to develop tools, deploying automation solutions that saved 250+ labor hours monthly.",
+		className: "md:col-span-2",
+		thumbnail: "/exp1.png",
+	},
+	{
+		id: 2,
+		company: "Manning College of Information & Computer Sciences",
+		title: "Software Architect",
+		dateStart: "Jan. 2023",
+		dateEnd: "May. 2023",
+		desc: "Partnered with a professor to build a PaaS, streamlining project-based learning for 200+ students with enhanced data management.",
+		className: "md:col-span-2", // change to md:col-span-2
+		thumbnail: "/exp2.png",
+	},
+	{
+		id: 3,
+		company: "U-Tutors",
+		title: "Lead Full Stack Developer",
+		dateStart: "Jan. 2023",
+		dateEnd: "May. 2023",
+		desc: "Engineered a student-to-student tutoring platform, boosting user engagement by 30% through a user-friendly interface and targeted marketing.",
+		className: "md:col-span-2", // change to md:col-span-2
+		thumbnail: "/exp3.jpeg",
+	},
+	{
+		id: 4,
+		company: "GRUBBRR",
+		title: "Full Stack Developer Intern",
+		dateStart: "Jun. 2022",
+		dateEnd: "Dec. 2022",
+		desc: "Developed and launched a Menu Builder Tool with 10+ features, improving system performance by 20% with Svelte.",
+
+		className: "md:col-span-2",
+		thumbnail: "/exp4.jpeg",
+	},
+	{
+		company: "School of Public Health & Health Sciences",
+		title: "IT Assistant",
+		dateStart: "Jan. 2022",
+		dateEnd: "May. 2022",
+		desc: "Supported and maintained 400+ in-house computer systems for the School of Public Health using ServiceNow and Tenable.io for vulnerability detection.",
+		className: "md:col-span-2",
+		thumbnail: "/exp5.jpg",
+	},
+	{
+		id: 6,
+		company: "Tata Consultancy Services Limited",
+		title: "Business Analyst Intern",
+		dateStart: "Jun. 2021",
+		dateEnd: "Aug. 2021",
+		desc: "Analyzed L'Oréal’s time series data with Python, providing insights that guided 12+ product development decisions with 93% accuracy.",
+		className: "md:col-span-2",
+		thumbnail: "/exp1.png",
+	},
+	{
+		id: 7,
+		company: "College of Social and Behavioral Sciences",
+		title: "Fullstack Developer",
+		dateStart: "Jun. 2021",
+		dateEnd: "Aug. 2021",
+		desc: "Led a team of 5+ developers to create a web application, enhancing data management efficiency by 15% with interactive visualizations.",
+		className: "md:col-span-2",
+		thumbnail: "/exp7.jpg",
+	},
+	{
+		id: 8,
+		company: "University of Massachusetts Amherst",
+		title: "Resident Assistant",
+		dateStart: "Jan. 2021",
+		dateEnd: "Jan. 2022",
+		desc: "Supported students' learning and growth by fostering inclusive communities, creating opportunities, and maintaining safe, well-kept residences.",
+
+		className: "md:col-span-2",
+		thumbnail: "/exp2.png",
+	},
+];
+
+export const socialMedia = [
+	{
+		id: 1,
+		img: "/link.svg",
+		link: "https://www.linkedin.com/in/roshanpshetty/",
+	},
+];
