1b036f052c33f169c9e442792a203a13c9605933
Roshan Praveen Shetty <roshanshetty2000@gmail.com>
2024-08-16 09:58:40 -0400

    Implemented download section

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+import Download from "@/components/Download";
 import { FeaturesSectionDemo } from "@/components/FeaturesDemo";
 import Footer from "@/components/Footer";
 import { HeroScrollDemo } from "@/components/HeroScrollDemo";
@@ -8,6 +9,7 @@ export default function Home() {
 	return (
 		<main className="relative flex justify-center items-center flex-col mx-auto sm:px-10 px-5 overflow-clip">
 			<HeroScrollDemo />
+			<Download />
 			<FeaturesSectionDemo />
 			<InfiniteMovingCardsDemo />
 			<Footer />
---
+++ components/Download.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import MagicButton from "./ui/MagicButton";
+import { FaLocationArrow } from "react-icons/fa";
+
+const Download = () => {
+	return (
+		<div className="flex flex-col items-center">
+			<h1 className="text-4xl lg:text-5xl lg:leading-tight max-w-5xl mx-auto text-center tracking-tight font-semibold text-black dark:text-white">
+				Transform Your <span className="text-red-700 font-bold">Netflix</span>{" "}
+				Experience Today!
+			</h1>
+			<p className="text-sm lg:text-base  max-w-2xl  mt-4 mx-auto text-neutral-500 text-center font-normal dark:text-neutral-300">
+				Download AutoSkip on the Chrome Web Store and never miss a beat. Enjoy
+				seamless streaming with just a click!
+			</p>
+			<a href="/">
+				<MagicButton
+					title="Install Now"
+					icon={<FaLocationArrow />}
+					position="right"
+				/>
+			</a>
+		</div>
+	);
+};
+
+export default Download;
