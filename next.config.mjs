1a606247836265bcdf45d32390e4477496c2e74c
Roshan Praveen Shetty <roshanshetty2000@gmail.com>
2024-08-22 07:49:59 -0400

    added images and packages

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	images: {
-		domains: ["lh3.googleusercontent.com"], // Add the domain for external images
+		domains: ["lh3.googleusercontent.com", "images.unsplash.com"], // Add the domain for external images
 	},
 	webpack(config) {
 		// Grab the existing rule that handles SVG imports
