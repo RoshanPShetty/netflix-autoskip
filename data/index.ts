5d95e0e3c722de5885823134a1c322dd18b08c8c
Roshan Praveen Shetty <roshanshetty2000@gmail.com>
2024-08-22 06:00:32 -0400

    Implemented Header

--- data/index.ts
+++ data/index.ts
@@ -4,15 +4,73 @@ export const header = [
 		path: "#home",
 	},
 	{
-		title: "Projects",
-		path: "#projects",
+		title: "Features",
+		path: "#features",
 	},
 	{
-		title: "About",
-		path: "#about",
+		title: "Reviews",
+		path: "#reviews",
 	},
 	{
 		title: "Contact",
 		path: "#contact",
+	},
+];
+
+export const socials = [
+	{
+		id: 1,
+		name: "Github",
+		img: "/github.svg",
+		link: "https://github.com/roshanpshetty",
+	},
+	{
+		id: 2,
+		name: "LinkedIn",
+		img: "/linkedin.svg",
+		link: "https://www.linkedin.com/in/roshanpshetty/",
+	},
+	{
+		id: 3,
+		name: "Twitter",
+		img: "/insta.svg",
+		link: "https://x.com/roshanpshetty",
+	},
+];
+
+export const reviews = [
+	{
+		profilePic:
+			"https://lh3.googleusercontent.com/a/ACg8ocLLAlrgZivuRKuHphPPPEl5eRSjLBz7U4cvFqkO8VoUb6exEA=s48-w48-h48",
+		username: "Lumina",
+		rating: "5 out of 5 stars",
+		date: "Aug 19, 2024",
+		reviewText:
+			"Fantastic! It skips intros and episodes effortlessly, making binge-watching a breeze. Super easy to use and it just works—love it!",
+	},
+	{
+		profilePic:
+			"https://lh3.googleusercontent.com/a-/ALV-UjUgYC8VHvrKx4XxeBefPSy4qyZL7OItHKyJpaxhA4TkLMVz2w1m=s48-w48-h48",
+		username: "Rishita Shetty",
+		rating: "5 out of 5 stars",
+		date: "Aug 20, 2024",
+		reviewText:
+			"I really like the extension, it works really well. Hope you make more of these for other apps!",
+	},
+	{
+		profilePic:
+			"https://lh3.googleusercontent.com/a-/ALV-UjWffiHY4nG88Qo4DdzJZOK3zaAYd-VmhtrYFt3Dkk6QLKM_Bes=s48-w48-h48",
+		username: "Akemi Jin",
+		rating: "5 out of 5 stars",
+		date: "Aug 19, 2024",
+		reviewText: "Please create another one for Peacock and Hulu!",
+	},
+	{
+		profilePic:
+			"https://lh3.googleusercontent.com/a-/ALV-UjVYLJpfapRYQVPXxlNvaDez9nqN5zVk4bqI28aLNGUpM8EpERs=s48-w48-h48",
+		username: "Archie Mannu",
+		rating: "5 out of 5 stars",
+		date: "Aug 19, 2024",
+		reviewText: "Great Extension! Simple but very effective",
 	},
 ];
